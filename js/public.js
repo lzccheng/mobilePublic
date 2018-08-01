54fa64854279b7489d90fc752fc2d9bb7981996a
lzccheng <Lai.zhicheng@yahoo.com>
2018-08-01 17:52:54 +0800

    'first'

---
+++ js/public.js
@@ -0,0 +1,6 @@
+$(function(){
+	$(window).resize(function(){
+		$('html').css('font-size',$('html').css('width').slice(0,-2)*2/7.5);
+	})
+	$('html').css('font-size',$('html').css('width').slice(0,-2)*2/7.5);
+});
